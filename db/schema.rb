22f5a38ff1562a23c96d17d3597a2a145389cab9
Nima Azarbayjani <nimaza@gmail.com>
2020-12-07 08:01:41 +0330

    created the carts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_042937) do
+ActiveRecord::Schema.define(version: 2020_12_07_042258) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
