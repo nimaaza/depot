8e0033df509267368d36a7c0ed0e1903bba070e4
Nima Azarbayjani <nimaza@gmail.com>
2020-12-07 08:27:53 +0330

    added associations for cart and product model using the line_item model

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,17 @@ class Product < ApplicationRecord
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'line items present')
+      throw :abort
+    end
+  end
 end
