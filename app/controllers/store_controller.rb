4a79204806481c95ae31e5beb527f4fff5e44613
Nima Azarbayjani <nimaza@gmail.com>
2020-12-08 07:39:08 +0330

    added counter to the store index to count the number of visits to the page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
   def index
+    session[:counter].present? ? session[:counter] += 1 : session[:counter] = 1
+    @counter = session[:counter]
+
     @products = Product.order(:title)
   end
 end
