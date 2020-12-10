a273f634de06bf4dda57272a94e5a9cd100e1280
Nima Azarbayjani <nimaza@gmail.com>
2020-12-10 17:01:21 +0330

    moved cart to the sidebar on the store index page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,11 @@
 class StoreController < ApplicationController
+  include CurrentCart
+
+  before_action :set_cart
+
   def index
     session[:counter].present? ? session[:counter] += 1 : session[:counter] = 1
     @counter = session[:counter]
-
     @products = Product.order(:title)
   end
 end
