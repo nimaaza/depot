125d851f3b8904da3f2f1c81ea423c9da3ad21a1
Nima Azarbayjani <nimaza@gmail.com>
2020-12-13 12:05:04 +0330

    reduce line_item's quantity by one instead of destroying the whole line_itm and destroy it only when quantity reaches zero

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -60,7 +60,13 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @line_item.destroy
+    @line_item.quantity -= 1
+    if @line_item.quantity.zero?
+      @line_item.destroy
+    else
+      @line_item.save
+    end
+
     respond_to do |format|
       format.html { redirect_to store_index_url }
       format.js
