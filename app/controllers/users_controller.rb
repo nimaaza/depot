26212e61bb29e0ace8a57b4369c3959350d9a26a
Nima Azarbayjani <nimaza@gmail.com>
2020-12-16 19:22:24 +0330

    enable admin users to login + prevent administrative parts from being access with an admin logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,16 +59,20 @@ class UsersController < ApplicationController
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
+
+    rescue_from 'User::Error' do |exception|
+      redirect_to users_url, notice: exception.message
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :password, :password_confirmation)
-    end
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
