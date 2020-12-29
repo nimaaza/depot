9969fc258ecb0567780b96e21d15e4b6c03dccfa
Nima Azarbayjani <nimaza@gmail.com>
2020-12-29 14:56:32 +0330

    send emails to customers upon completing their purchase

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
+    user_name: 'nimaza',
+    password: 'secret',
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
